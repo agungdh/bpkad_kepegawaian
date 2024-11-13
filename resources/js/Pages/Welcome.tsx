dbdbccc79138f5577f32da54858509c4e2aa33ad
AgungDH <agungdh@live.com>
2024-11-13 14:54:47 +0700

    vite config listen local 3000

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -69,8 +69,6 @@ export default function Welcome({
                             </nav>
                         </header>
 
-                        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
-
                         <main className="mt-6">
                             <div className="grid gap-6 lg:grid-cols-2 lg:gap-8">
                                 <a
