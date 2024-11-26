9da8ad07f4ec9fad345cbd00fb6ce5fe4f132fac
AgungDH <agungdh@live.com>
2024-11-26 14:38:23 +0700

    WIP: layout

--- resources/js/Layouts/DefaultLayout.tsx
+++ resources/js/Layouts/DefaultLayout.tsx
@@ -1,11 +1,44 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
-import { PropsWithChildren } from 'react';
+import { AppSidebar } from "@/Components/app-sidebar"
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/Components/ui/breadcrumb"
+import { Separator } from "@/Components/ui/separator"
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "@/Components/ui/sidebar"
+import { PropsWithChildren } from "react"
 
-export default function DefaultLayout({ children }: PropsWithChildren) {
+export default function DefaultLayout({ children }: PropsWithChildren<{}>) {
     return (
-        <>
-            {children}
-        </>
-    );
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
+                    <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem className="hidden md:block">
+                                <BreadcrumbLink href="#">
+                                    Building Your Application
+                                </BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator className="hidden md:block" />
+                            <BreadcrumbItem>
+                                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                    </Breadcrumb>
+                </header>
+                {children}
+            </SidebarInset>
+        </SidebarProvider>
+    )
 }
