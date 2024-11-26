4383326be4f2a7f9fe14f893b2914570c6786109
AgungDH <agungdh@live.com>
2024-11-26 11:16:40 +0700

    antd

--- resources/js/Pages/Test.tsx
+++ resources/js/Pages/Test.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/Components/ui/button";
-
+import { Button as AButton } from "antd";
 export default function Dashboard() {
     return (
         <>
             <p>tehe</p>
             <Button>Tehee</Button>
+            <AButton type="primary">ANT Button</AButton>
         </>
     );
 }
